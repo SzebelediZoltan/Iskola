62b7393a65ce5ef9a439694ce6897d7e7a6828fa
Szebelédi Zoltán <szebeledi.zoltan@ckik.loc>
2024-05-10 11:09:23 +0200

    Szamologep vege

--- DANIEL/Alkalmazas/2024_05_07_objektumGeneralasa/2024_05_07_objektumGeneralasa/Form1.cs
+++ DANIEL/Alkalmazas/2024_05_07_objektumGeneralasa/2024_05_07_objektumGeneralasa/Form1.cs
@@ -12,19 +12,14 @@ namespace _2024_05_07_objektumGeneralasa
 {
     public partial class Form1 : Form
     {
+        static double osszeg = 0;
+        static int muvelet = 1;
         public Form1()
         {
             InitializeComponent();
             SzamokGeneralasa();
             textBox1.TextAlign = HorizontalAlignment.Right;
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //SzamokGeneralasa();
-
-        }
-
         private void SzamokGeneralasa()
         {
             int n = 3;
@@ -32,10 +27,12 @@ namespace _2024_05_07_objektumGeneralasa
             {
                 for (int i = 0; i < n; i++)
                 {
-                    GombKeszites(12 + 60 * i, 120 + 60 * j, 60, 60, "" + (j * n + i + 1));
+                    GombKeszites(12 + 60 * (i+1), 120 + 60 * j, 60, 60, "" + (j * n + i + 1));
                 }
             }
-            GombKeszites(12 + 60 * 1, 120 + 60 * n, 60, 60, "0");
+            GombKeszites(12 + 60 * 1, 120 + 60 * n, 60, 60, "C");
+            GombKeszites(12 + 60 * 2, 120 + 60 * n, 60, 60, "0");
+            GombKeszites(12 + 60 * 3, 120 + 60 * n, 60, 60, ",");
         }
 
         private void GombKeszites(int x, int y, int w, int h, string v)
@@ -52,8 +49,92 @@ namespace _2024_05_07_objektumGeneralasa
         {
             Button gomb = (Button) sender;
             //MessageBox.Show(gomb.Text, "Gomb értéke");
-            
-            textBox1.Text += gomb.Text;
+            if (muvelet == 5)
+            {
+                textBox1.Text = "";
+                muvelet = 1;
+            }
+            if (gomb.Text == "C")
+            {
+                textBox1.Text = "";
+                osszeg = 0;
+                muvelet = 1;
+            } else
+            {
+                textBox1.Text += gomb.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ElozoMuvelet();
+            textBox1.Text = "";
+            muvelet = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ElozoMuvelet();
+            textBox1.Text = Convert.ToString(osszeg);
+            osszeg = 0;
+            muvelet = 5;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ElozoMuvelet();
+            textBox1.Text = "";
+            muvelet = 2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ElozoMuvelet();
+            textBox1.Text = "";
+            muvelet = 3;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ElozoMuvelet();
+            textBox1.Text = "";
+            muvelet = 4;
+        }
+
+        private void ElozoMuvelet()
+        {
+            switch (muvelet)
+            {
+                case 1:
+                    osszeg += Convert.ToDouble(textBox1.Text);
+                    break;
+                case 2:
+                    osszeg -= Convert.ToDouble(textBox1.Text);
+                    break;
+                case 3:
+                    osszeg = osszeg * Convert.ToDouble(textBox1.Text);
+                    break;
+                case 4:
+                    osszeg = osszeg / Convert.ToDouble(textBox1.Text);
+                    break;
+                case 5:
+                    osszeg += Convert.ToDouble(textBox1.Text);
+                    break;
+            }
+        }
+
+        private void Szam(object sender, EventArgs e)
+        {
+            string szam = "";
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (Char.IsDigit(textBox1.Text[i]) || textBox1.Text[i] == ',')
+                {
+                    szam += textBox1.Text[i];
+                }
+            }
+
+            textBox1.Text = szam;
         }
     }
 }
