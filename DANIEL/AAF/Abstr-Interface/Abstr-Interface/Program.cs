936eddb44142649c40467b52dc59e95c8a4cf3ba
Szebelédi Zoltán <szebeledi.zoltan@ckik.loc>
2024-10-15 09:42:41 +0200

    teszt

---
+++ DANIEL/AAF/Abstr-Interface/Abstr-Interface/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstr_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+    abstract class A
+    {
+        public int x;
+        public string sz;
+
+        public A(string sz, int x)
+        {
+            this.sz = sz;
+            this.x = x;
+        }
+
+        public string Ismetel()
+        {
+            string er = "";
+            for (int i = 0; i < x; i++)
+            {
+                er += sz;
+            }
+            return er;
+        }
+        public abstract void Szamol();
+    }
+
+    class B : A
+    {
+        public B() : base("alma", 5)
+        {
+            
+        }
+        public override void Szamol()
+        {
+            x = x * 10;
+        }
+    }
+}
